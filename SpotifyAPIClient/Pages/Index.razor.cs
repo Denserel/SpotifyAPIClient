513fb8374b753383acce9711a6b517a2628f8332
denserel <Denzerel@gmail.com>
2021-11-30 11:50:54 +0100

    Added try, catch for new releases

--- SpotifyAPIClient/Pages/Index.razor.cs
+++ SpotifyAPIClient/Pages/Index.razor.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components;
 using SpotifyAPIClient.APIService;
 using SpotifyAPIClient.Dtos;
+using SpotifyAPIClient.Models;
 
 namespace SpotifyAPIClient.Pages;
 
@@ -10,19 +11,33 @@ public partial class Index
     [Inject]
     IApiService apiService { get; set; }
     
-    private IEnumerable<NewReleaseDto> release;
+    private IEnumerable<NewReleaseDto> releases;
     
     protected override async Task OnInitializedAsync()
     {
-        var newReleases = await apiService.GetNewReleases();
-        
-        release = newReleases.albums.items.Select(item => new NewReleaseDto
+        try
+        {
+            var newReleases = await apiService.GetNewReleases();
+            releases = NewReleaseToDto(newReleases);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+
+    private IEnumerable<NewReleaseDto> NewReleaseToDto(NewReleases newReleases)
+    {
+        var releases = newReleases.albums.items.Select(item => new NewReleaseDto
         {
             ImageUrl = item.images.FirstOrDefault().url,
             Name = item.name,
             Date = item.release_date,
-            Artist = string.Join(",", item.artists.Select( artist => artist.name))
+            Artist = string.Join(",", item.artists.Select(artist => artist.name))
         });
+
+        return releases;
     }
 }
 
